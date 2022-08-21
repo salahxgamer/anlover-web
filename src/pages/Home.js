4c01d30cc157429a7f109dc0f120b2ef1aec35a0
salahxgamer <salahxgamer2015@gmail.com>
2022-08-21 16:54:24 +0100

    Added Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { CollectionPlayFill, Play } from 'react-bootstrap-icons';
+
+const Home = () => {
+    return (
+        <>
+            <header>
+                <Container className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh" }}>
+                    <h1>Welcome to AnLover</h1>
+                    <h4>The free Anime Lovers space</h4>
+                    <hr />
+                    <Button variant="outline-primary" size="lg" className="mt-3 px-4" href="#one"><Play size={30} /> Start</Button>
+                </Container>
+            </header>
+            <section id="one">
+                <Container className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh" }}>
+                    <Row className="bg-primary text-white p-5" style={{ height: "60%" }} >
+                        <Col m className="d-flex align-items-center justify-content-center">
+                            <CollectionPlayFill size={100} />
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-center">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </>
+    );
+}
+
+export default Home;
