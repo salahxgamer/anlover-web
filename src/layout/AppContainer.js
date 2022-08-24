ebe88cf3edf12ea2a9f028eb1207ae11a92a0340
salahxgamer <salahxgamer2015@gmail.com>
2022-08-24 18:52:32 +0100

    Added SideBar to AppContainer Added proptypes for AppContainer

--- src/layout/AppContainer.js
+++ src/layout/AppContainer.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+import { Container, Row, Col } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import NavBar from './NavBar';
+import SideBar from './SideBar';
 
-export default function AppContainer({ children }) {
+export default function AppContainer({ showNavBar, showSideBar, showFooter, children }) {
     return (
         <Container fluid="true">
-            <NavBar />
-            <Container>
-                <Outlet />
-                {children}
+            {showNavBar && <NavBar />}
+            <Container fluid="true">
+                <Row className="flex-nowrap gx-0">
+                    <Col xs="auto">
+                        {showSideBar && <SideBar />}
+                    </Col>
+                    <Col className="overflow-auto">
+                        <Outlet />
+                        {children}
+                    </Col>
+                </Row>
             </Container>
-            <Footer />
+            {showFooter && <Footer />}
         </Container>
-    )
+    );
 }
+
+AppContainer.defaultProps = {
+    showNavBar: true,
+    showSideBar: true,
+    showFooter: true,
+};
+
+AppContainer.propTypes = {
+    showNavBar: PropTypes.bool,
+    showSideBar: PropTypes.bool,
+    showFooter: PropTypes.bool,
+    children: PropTypes.node,
+};
