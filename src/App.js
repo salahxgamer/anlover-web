876474fbd68ec07d3cd37d1a3e0c47352fb250bb
salahxgamer <salahxgamer2015@gmail.com>
2022-08-24 18:50:10 +0100

    Added react-bootstrap-sidebar-menu package Added main scss file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "./styles/App.scss";
 
 import { AuthProvider } from "./contexts/AuthContext"
 import Signup from "./pages/Signup"
