c286d05872e6cbdd3c81902b09efb70593790393
salahxgamer <salahxgamer2015@gmail.com>
2022-09-01 12:27:33 +0100

    Added API:getEpisode temporary method

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -54,4 +54,81 @@ export default class API {
             .then(response => response.response)
             .catch((error, data) => { console.error(error, data); return {} })
     }
+
+    /**
+     * @param  {} episode_id Episode id
+     */
+    static getEpisode(episode_id) {
+        const urlencoded = new URLSearchParams();
+        urlencoded.append("json", JSON.stringify({ episode_id }));
+
+        return fetch(`${this.BASE_URL}/episodes/get-episodes?${urlencoded}`, { method: 'GET', headers: this.AUTH_HEADERS })
+            .then(response => response.json())
+            .then(response => response.response.data[0])
+            .then(async response => { response.episode_sources = await this.getEpisodeSources(response); return response })
+            .catch((error, data) => { console.error(error, data); return {} })
+    }
+
+    static async getEpisodeSources(episode) {
+
+        // Get the providers urls from api
+        let providersURL = [];
+        for (let srcProvider of episode.episode_urls) {
+            const srcProviderURL = srcProvider.episode_url;
+            try {
+                let data = await fetch(this.bypassCORS(srcProviderURL), { method: 'GET', headers: this.AUTH_HEADERS })
+                data = await data.json()
+
+                // console.log(data)
+                providersURL = providersURL.concat(data)
+            } catch (error) {
+                console.warn("failed loading from : ", srcProvider, error)
+            }
+        }
+
+        const providers = providersURL.reduce((providers, source, index) =>
+            providers.concat([{
+                id: index, provider_url: source, provider_host: "", provider_type: "", provider_name: new URL(source).host, provider_urls: []
+            }]
+            ), []);
+
+
+        // evaluate providers urls and scrape url sources
+        for (const [index, provider] of providers.entries()) {
+
+            const url = this.bypassCORS(provider.provider_url)
+            const options = {
+                redirect: "follow",
+                method: "post"
+            }
+
+            let result;
+            try {
+                let rsp = await fetch(url, options)
+
+                // console.log("loaded :", rsp.status, await rsp);
+                let content = await rsp.text();
+                result = Decoder.Decoder.decode(rsp.url, await content);
+
+            } catch (error) {
+                console.error("failed scraping : ", provider, error)
+            }
+
+            if (result)
+                providers[index] = {
+                    ...provider,
+                    provider_host: result.host,
+                    provider_type: result.type,
+                    provider_urls: result.urls
+                }
+            // console.log(result);
+        }
+
+        return providers
+
+    }
+
+    static bypassCORS(url) {
+        return `/proxy/${url}`;
+    }
 }
