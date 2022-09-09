52b273060faccd0a953cc5d956b979f12e4af05e
salahxgamer <salahxgamer2015@gmail.com>
2022-09-09 15:16:55 +0100

    Added ErrorBoundary component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        console.error(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return (this.props.fallback ?? <div className="h-100 d-flex align-items-center justify-content-center"><h1>Something went wrong.</h1></div>);
+        }
+
+        return this.props.children;
+    }
+}
+export default ErrorBoundary;
