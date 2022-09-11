b86c93a410b3b80bbafce5cecef4f1e8d0729799
salahxgamer <salahxgamer2015@gmail.com>
2022-09-11 21:03:56 +0100

    Updated Brand with new logo

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { ChatLeftHeart } from 'react-bootstrap-icons'
+import anlover_logo from '../assets/anlover_logo.svg'
 
 export default function Brand() {
     return (
         <span>
-            AnLover
-            <ChatLeftHeart color="red" />
+            <img src={anlover_logo} alt="AnLover" width="20" height="20" className="mx-1" style={{ verticalAlign: "-2px" }} />
+            Anlover
             <small className="mx-1 fs-6 text-muted"><em>v0.1.0</em></small>
         </span>
     )
