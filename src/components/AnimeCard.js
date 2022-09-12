2a5521a7cd4f3c1d8e9bd4ccb1085787a2382e5e
salahxgamer <salahxgamer2015@gmail.com>
2022-09-12 16:46:30 +0100

    Enhanced anime cards

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,49 +1,69 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { OverlayTrigger, Tooltip, Placeholder } from 'react-bootstrap'
+import { Placeholder } from 'react-bootstrap'
+import { PlayFill, CheckCircleFill, PlayCircleFill, ClockFill, PauseCircleFill, XCircleFill, EyeSlashFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 import '../styles/AnimeCard.scss'
 
 function AnimeCard(props) {
-    const { anime, placeholder, hideCover } = props;
+    const { anime, placeholder } = props;
+
+    // TODO: isolate this to be used globally
+    const WATCH_STATUS_ICONS = {
+        "watched": CheckCircleFill,
+        "watching": PlayCircleFill,
+        "plan_to_watch": ClockFill,
+        "on_hold": PauseCircleFill,
+        "dropped": XCircleFill,
+    }
+
+    // TODO: add watch status when the api supports it
+    const WatchStatusIcon = WATCH_STATUS_ICONS["status"] || EyeSlashFill
 
     return (
-        /* Anime card wrapper */
-        <Link to={placeholder ? "" : `/anime/${anime.anime_id}`} className="text-decoration-none text-reset">
-            <div className="anime-card h-100 d-flex flex-column align-items-center shadow rounded overflow-hidden">
+        <div className="anime-card h-100 w-100 rounded shadow overflow-hidden">
 
+            <div className="anime-poster">
 
-                {/* Anime cover */}
-                {!(hideCover || placeholder) && <div className="cover" style={{ backgroundImage: `url(${anime.anime_cover_image_url})` }} />}
-                {placeholder && !hideCover && < Placeholder animation="glow"><Placeholder className="cover" /></Placeholder>}
+                {!placeholder && <>
+                    <div className="tags ltr d-none d-sm-block">
+                        <div className="badge text-bg-light">{anime.anime_type}</div>
+                        <div className="badge text-bg-light">DUB</div>
+                    </div>
+                    <div className="tags rtl">
+                        <WatchStatusIcon size="1rem" className="text-light" />
+                        <div className="badge text-bg-success">{anime.anime_rating}</div>
+                    </div>
+                </>}
 
-                {/* Anime details */}
-                <div className="p-2 w-100">
-                    {/* Generate tooltip containing full anime title */}
-                    <OverlayTrigger
-                        placement="bottom"
-                        overlay={<Tooltip>{anime.anime_name}</Tooltip>}
-                    >
-                        <h5 className="text-truncate">
-                            {placeholder
-                                ? <Placeholder animation="glow"><Placeholder xs={6} /> <Placeholder xs={2} /></Placeholder>
-                                : anime.anime_name
-                            }
-                        </h5>
-                    </OverlayTrigger>
+                {!placeholder && <img src={anime.anime_cover_image_url} className="anime-poster-img" alt={anime.anime_name} />}
+                <Link to={placeholder ? "" : `/anime/${anime.anime_id}`} className="anime-poster-ahref">
+                    <PlayFill className="play" size={60} />
+                </Link>
+            </div>
 
-                    {placeholder ? <Placeholder animation="glow" className="hstack gap-3"><Placeholder xs={2} /> <Placeholder xs={5} /> <Placeholder xs={3} /></Placeholder> :
+            <div className="anime-detail">
+                <h3 className="anime-name">
+                    <Link to={placeholder ? "" : `/anime/${anime.anime_id}`} title={anime.anime_name}>
+                        {placeholder
+                            ? <Placeholder animation="glow"><Placeholder xs={6} /> <Placeholder xs={4} /> <Placeholder xs={2} /> <Placeholder xs={3} /></Placeholder>
+                            : anime.anime_name
+                        }
+                    </Link>
+                </h3>
 
-                        <div className="hstack gap-3">
-                            <span className="badge text-bg-primary rounded-pill">{anime.anime_rating}</span>
-                            <span className="badge text-bg-secondary">{anime.anime_status}</span>
-                            <span className="badge text-bg-info">{anime.anime_release_year}</span>
-                        </div>
-                    }
-                </div>
+                {placeholder
+                    ? <Placeholder className="anime-info" animation="glow"><Placeholder xs={4} /> <Placeholder xs={3} /> <Placeholder xs={2} /></Placeholder>
+                    : < div className="anime-info">
+                        <span>{anime.anime_status}</span>
+                        <span>{anime.anime_release_year}</span>
+                        <span>{anime.anime_season}</span>
+                    </div>
+                }
 
-            </div >
-        </Link >
+            </div>
+        </div >
+
     )
 }
 
