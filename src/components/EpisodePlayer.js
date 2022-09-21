461b06fc61fede5fff81dddea29cc39459443b35
salahxgamer <salahxgamer2015@gmail.com>
2022-09-21 17:14:33 +0100

    New ProviderSelector

--- src/components/EpisodePlayer.js
+++ src/components/EpisodePlayer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
 import ProviderSelector from '../components/ProviderSelector';
+import Provider from '../classes/Provider';
 
 export default class EpisodePlayer extends Component {
     static propTypes = {
@@ -11,25 +12,49 @@ export default class EpisodePlayer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            providers: props.providers,
+            providers: props.providers.map(provider => new Provider(provider)),
             urlIndex: -1,
-            selectedProvider: null
+            selectedProvider: null,
         }
+    }
+
+    componentDidMount() {
+        const { providers } = this.state;
+        let autoSelectedProvider = null;
+        providers.forEach(provider => {
+            provider.resolve().finally(() => this.forceUpdate());
+            provider.onSuccessfulResolve((provider) => {
+                if (!autoSelectedProvider) {
+                    autoSelectedProvider = provider;
+                    this.selectProvider(autoSelectedProvider);
+                }
+            });
+        })
+    }
+
+    componentWillUnmount() {
+        const { providers } = this.state;
+        providers.forEach(provider => provider.cancel())
+    }
+
+    selectProvider = (selectedProvider, urlIndex = -1) => {
+        this.setState({ selectedProvider, urlIndex })
     }
 
     render() {
         const { providers, selectedProvider, urlIndex } = this.state
-        const { providers: _, ...props } = this.props
+        // eslint-disable-next-line no-unused-vars
+        const { providers: _, ...newProps } = this.props
         return (
-            <div className="shadow rounded w-100 bg-dark d-flex flex-column align-items-stretch" style={{ maxHeight: "90vh" }} {...props}>
+            <div className="shadow rounded w-100 bg-dark d-flex flex-column align-items-stretch" style={{ maxHeight: "90vh" }} {...newProps}>
                 <ReactPlayer url={selectedProvider?.urls?.at(urlIndex)} playing controls width="100%" height="100%" className="rounded-top overflow-hidden" />
 
                 <div>
                     <ProviderSelector
                         providers={providers}
-                        onLoad={(provider) => provider.urls.length && (selectedProvider || this.setState({ selectedProvider: provider }))}
-                        onSelect={(selectedProvider, urlIndex) => this.setState({ selectedProvider, urlIndex })}
-                        className="" />
+                        onSelect={this.selectProvider}
+                        selectedProvider={selectedProvider}
+                    />
                 </div>
             </div>
         )
--- src/components/ProviderSelector.js
+++ src/components/ProviderSelector.js
@@ -1,43 +1,97 @@
-import { Dropdown } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import Provider from './Provider';
+import { Badge, Spinner } from 'react-bootstrap';
+import { XCircleFill } from 'react-bootstrap-icons';
+import Select, { components, createFilter } from 'react-select';
+import Provider from '../classes/Provider';
+
+
+
 
 /**
- * @typedef {object} Provider
- * @property {string} name - provider name
- * @property {string} url - provider url
- * @property {array<string>} urls - provider source urls
- * 
- * @typedef {object} Props
- * @property {array<Provider>} providers - array of providers
- * @property {function} onSelect - callback function when a provider is selected
- * @property {function} onLoad - callback function when a provider is fetched
- * @property {object} props - other props
+ * @typedef {Object} Props
+ * @property {Array<Provider>} providers - array of providers to select from
+ * @property {Provider} selectedProvider - the selected provider
+ * @property {Function} onSelect - callback function when a provider is selected
+ * @property {Object} otherProps - other props
  * 
  * @param {Props} props
  */
-export default function ProviderSelector({ providers, onSelect, onLoad, ...props }) {
-
+export default function ProviderSelector(props) {
+    const { providers, selectedProvider, onSelect, ...otherProps } = props
     return (
-        <Dropdown {...props}>
-            <Dropdown.Toggle variant="dark">
-                Select Provider
-            </Dropdown.Toggle>
-            <Dropdown.Menu renderOnMount style={{ overflowY: "auto", maxHeight: "18rem" }}>
-                {
-                    providers?.map((provider, index) => <Provider {...provider} onLoad={onLoad} onSelect={onSelect} key={index} />)
-                }
-            </Dropdown.Menu>
-        </Dropdown >
+        <Select
+            menuPlacement="auto"
+            isClearable={false}
+            isMulti={false}
+            menuShouldScrollIntoView={false}
+            isSearchable
+
+            aria-autocomplete="both"
+            placeholder="Select a provider..."
+            noOptionsMessage={({ inputValue }) => `No providers containing : ${inputValue}`}
+            filterOption={createFilter({
+                ignoreAccents: true,
+                ignoreCase: true,
+                stringify: option => option.label,
+            })}
+
+            // Put all the successful providers on top and others down
+            options={providers.sort((provider) => (provider.isResolved ? (provider.isSuccessful ? -1 : 1) : 0))}
+            value={selectedProvider}
+            onChange={provider => { onSelect && onSelect(provider, -1) }}
+
+            getOptionLabel={provider => provider.name}
+            getOptionValue={provider => provider.toString()}
+            isOptionSelected={(provider, selectedProviders) => selectedProviders.find((selectedProvider) => selectedProvider === provider)}
+            isOptionDisabled={provider => !provider.isSuccessful}
+            isLoading={!!providers.find(provider => !provider.isResolved)}
+
+            components={{ Option: ProviderOption }}
+
+            {...otherProps}
+        />
     )
 }
 
+
 ProviderSelector.propTypes = {
-    providers: PropTypes.arrayOf(PropTypes.shape({
-        url: PropTypes.string.isRequired,
-        name: PropTypes.string,
-        urls: PropTypes.arrayOf(PropTypes.string)
-    })),
+    providers: PropTypes.arrayOf(PropTypes.instanceOf(Provider)).isRequired,
+    selectedProvider: PropTypes.instanceOf(Provider),
     onSelect: PropTypes.func,
-    onLoad: PropTypes.func
 }
+
+/**
+ * A React component that renders an option in the Select component.
+ * The component will render the provider details.
+ */
+const ProviderOption = ({ children, ...props }) => {
+    const provider = props.data;
+
+    const ProviderStatus = () => {
+        if (!provider.isResolved)
+            return <Spinner animation="grow" size="sm" variant="primary" />
+        if (!provider.isSuccessful)
+            return <XCircleFill className="text-danger" size="20" />
+        return (
+            <>
+                {provider.labels.map((label, index) => <Badge bg="primary" key={index}>{label}</Badge>)}
+                <Badge bg="success">{provider.urls.length}</Badge>
+            </>
+        )
+    }
+
+    return (
+        <components.Option className="provider-option" {...props}>
+            <div className='hstack gap-1'>
+                <div className='me-auto'>
+                    {children}
+                </div>
+                <ProviderStatus />
+            </div>
+        </components.Option>
+    );
+};
+
+ProviderOption.propTypes = {
+    children: PropTypes.node.isRequired
+};
