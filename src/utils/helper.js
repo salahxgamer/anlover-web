cbdd09aa3ad3594af8ee823ea1b3157fe16234a1
salahxgamer <salahxgamer2015@gmail.com>
2022-10-09 17:02:13 +0100

    Added snakeToTitleCase helper function Better JSDocs

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 
+
+/**
+ * Converts a snake_case string to Title Case.
+ *
+ * @param {string} string The string to convert.
+ * @return {string} The converted string.
+ */
+export function snakeToTitleCase(string) {
+    return string.replace(/^_*(.)|_+(.)/g, (_s, c, d) => c ? c.toUpperCase() : ' ' + d.toUpperCase());
+}
+
+
 /* This is a higher order component that 
 *  inject a special prop to our component.
 */
 export function withParams(Component) {
+    /**
+     * ComponentWithParams
+     * @param {object} props
+     * @returns {React.Component}
+     */
     function ComponentWithParams(props) {
         let params = useParams()
 
@@ -18,6 +35,11 @@ export function withParams(Component) {
 *  inject a special prop to our component.
 */
 export function withSearchParams(Component) {
+    /**
+     * ComponentWithSearchParams
+     * @param {Object} props
+     * @returns {React.Component}
+     */
     function ComponentWithSearchParams(props) {
         let [searchParams, setSearchParams] = useSearchParams()
 
